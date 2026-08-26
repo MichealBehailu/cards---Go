0f1e149a05dcc04fdbed56e6f89a517773087bb7
Micheal Behailu <michealbehailu94@gmail.com>
2026-08-26 11:51:07 +0300

    Varaible Declaration

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	// var card string = "Ace of Spades" //one way to define 
+	 card := "Ace of Spades" //this is the same as the above code //the compiler will figure it out (the type)
+	 card = "Five of Diamonds" //do assignment
+	 fmt.Println(card)
 }
