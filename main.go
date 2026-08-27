05d53561cec4faf0d74425676e1875193676864d
Micheal Behailu <michealbehailu94@gmail.com>
2026-08-27 15:46:12 +0300

    Custome type declaration

--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	cards := []string{"Ace of Diammonds", newCard()} //inside the brace we can put elements to it
-	cards = append(cards, "Six of Spades")                      //to add additional elements to it //it returns new slice so we can store it on the variable and it does not destroy the old one
+	cards := deck{"Ace of Diamonds", newCard()} //the deck is from the deck.go //it is slice //remeber when we run we have to include deck.go, that is go run main.go deck.go
+	cards = append(cards, "Six of Spades")      //to add additional elements to it //it returns new slice so we can store it on the variable and it does not destroy the old one
 
 	// fmt.Println(cards)
 
-	//to iterate over the slice
-	for i, card := range cards { //range keyword is used to iterate over every element in the slice
-		fmt.Println(i, card)
-	}
-	
+	cards.print()
+
 }
 
 func newCard() string {
