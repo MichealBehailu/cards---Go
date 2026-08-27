7ebd701bd3e46adcfe8a86241725819e2115697e
Micheal Behailu <michealbehailu94@gmail.com>
2026-08-27 16:28:51 +0300

    Creating a New Deck

--- deck.go
+++ deck.go
@@ -7,8 +7,23 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck { //return a value of deck
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"} //these are the variants
+	cardValues := []string{"Ace", "Two", "Three", "Four"}          //these are the values from ace to king
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() { //any variable of type 'deck' now gets access to the "print" method
-	for i, card:= range d {
+	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()} //the deck is from the deck.go //it is slice //remeber when we run we have to include deck.go, that is go run main.go deck.go
-	cards = append(cards, "Six of Spades")      //to add additional elements to it //it returns new slice so we can store it on the variable and it does not destroy the old one
-
-	// fmt.Println(cards)
-
+	cards := newDeck()
 	cards.print() //cards is passed as d in the deck.go //as a copy in short
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
