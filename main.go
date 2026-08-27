9bf266c1c24194c0af8b3267b349966271a548f3
Micheal Behailu <michealbehailu94@gmail.com>
2026-08-27 11:10:50 +0300

    Function and return type

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades" //one way to define 
-	 card := "Ace of Spades" //this is the same as the above code //the compiler will figure it out (the type)
-	 card = "Five of Diamonds" //do assignment
-	 fmt.Println(card)
+	// var card string = "Ace of Spades" //one way to define
+
+	card := newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
