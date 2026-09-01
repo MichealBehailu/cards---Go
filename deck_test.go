538f909cb88fa8519ecbce672fa9fc1bbaa0d76d
Micheal Behailu <michealbehailu94@gmail.com>
2026-09-01 10:11:11 +0300

    Writing useful tests

--- deck_test.go
+++ deck_test.go
@@ -1,2 +1,13 @@
 package main
 
+import "testing"
+
+func TestNewDeck(t *testing.T) { //t - is the test handler
+	d := newDeck()
+
+	//the number of cards inside should be 16 if not
+	if len(d) != 16 {
+		len := len(d)
+		t.Errorf("Expected deck length of 16, but got %v", len) //%v helps us to integrate the argument (len) with the string
+	}
+}
